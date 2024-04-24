938800db027ead5d6ba078abf16ae03ba9ed2c05
Jensen Nguyen <likeaboss24000@gmail.com>
2024-04-24 12:03:08 -0400

    Initial code

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,47 @@
     clippy::borrow_interior_mutable_const
 )]
 
-mod win1a;
-mod win2a;
-mod win3a;
-mod win1b;
-mod win3b;
+mod win1a_fushigisou;
+mod win1a_lizardon;
+mod win1a_zenigame;
+mod win1b_fushigisou;
+mod win1b_lizardon;
+mod win1b_zenigame;
 
-#[skyline::main(name = "robin_victoryfix_SL2")]
+mod win2a_fushigisou;
+mod win2a_lizardon;
+mod win2a_zenigame;
+mod win2b_fushigisou;
+mod win2b_lizardon;
+mod win2b_zenigame;
+
+mod win3a_fushigisou;
+mod win3a_lizardon;
+mod win3a_zenigame;
+mod win3b_fushigisou;
+mod win3b_lizardon;
+mod win3b_zenigame;
+
+#[skyline::main(name = "ptrainer_victoryfix_SL2")]
 pub fn main() {
-    win1a::install();
-    win2a::install();
-    win3a::install();
-    win1b::install();
-    win3b::install();
+    win1a_fushigisou::install();
+    win1a_lizardon::install();
+    win1a_zenigame::install();
+    win1b_fushigisou::install();
+    win1b_lizardon::install();
+    win1b_zenigame::install();
+
+    win2a_fushigisou::install();
+    win2a_lizardon::install();
+    win2a_zenigame::install();
+    win2b_fushigisou::install();
+    win2b_lizardon::install();
+    win2b_zenigame::install();
+
+    win3a_fushigisou::install();
+    win3a_lizardon::install();
+    win3a_zenigame::install();
+    win3b_fushigisou::install();
+    win3b_lizardon::install();
+    win3b_zenigame::install();
 }
